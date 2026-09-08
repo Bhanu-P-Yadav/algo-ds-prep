df197eb502558ca63bc433934c1982e50dafd553
Bhanu Pratap Yadav <workingwithbhanu24@gmail.com>
2026-09-09 00:03:38 +0530

    Time: 0 ms (100%), Space: 42.2 MB (19.23%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.java
+++ 1518-water-bottles/1518-water-bottles.java
@@ -1,12 +1,5 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
-        int solution=numBottles;
-        while(numBottles>=numExchange){
-            int newBottles=numBottles/numExchange;
-            int remBottles=numBottles%numExchange;
-            solution=solution+newBottles;
-            numBottles=newBottles+remBottles;
-        }
-        return solution;
+        return numBottles+(numBottles-1)/(numExchange-1);
     }
 }
